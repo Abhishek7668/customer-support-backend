6d01322645a4909c68e2c4fa94fc58b7d4aacb5a
Abhishek7668 <as9765025@gmail.com>
2026-07-20 11:46:48 +0530

    latest_change

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_origin_regex=r"https://frontend-d4c2.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
